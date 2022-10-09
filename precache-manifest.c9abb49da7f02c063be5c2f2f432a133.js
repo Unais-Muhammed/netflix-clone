f57cf6041fc65f00a7d6319b2e04426e40d4a277
Unais Muhammed <unaismuhammed001@gmail.com>
2022-10-09 12:01:01 +0530

    Updates

---
+++ precache-manifest.c9abb49da7f02c063be5c2f2f432a133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c97826f689356bfcb015",
+    "url": "/netflix-clone/static/css/main.2dbdcf7b.chunk.css"
+  },
+  {
+    "revision": "c97826f689356bfcb015",
+    "url": "/netflix-clone/static/js/main.c97826f6.chunk.js"
+  },
+  {
+    "revision": "66c154a22e2b7b2b9b37",
+    "url": "/netflix-clone/static/js/1.66c154a2.chunk.js"
+  },
+  {
+    "revision": "e09834afd7d054be4d37",
+    "url": "/netflix-clone/static/js/2.e09834af.chunk.js"
+  },
+  {
+    "revision": "9f255b38f0aa02e21e43",
+    "url": "/netflix-clone/static/js/runtime~main.9f255b38.js"
+  },
+  {
+    "revision": "3e14c2e0b34c93f5351771b009c03551",
+    "url": "/netflix-clone/index.html"
+  }
+];
